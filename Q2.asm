d5803848d1ca904e453ec5c8f86870b340c735ae
Shreyamaske15 <shreyamaske15@gmail.com>
2026-02-05 07:58:37 +0530

    Q2.asm

---
+++ Q2.asm
@@ -0,0 +1,32 @@
+ORG 0000H
+
+MOV R0, 50H       
+MOV R1, 51H        
+
+CHECK:
+MOV A, R0
+JZ A_ZERO          
+
+MOV A, R1
+JZ B_ZERO         
+
+DEC R0
+DEC R1
+SJMP CHECK
+
+A_ZERO:
+MOV A, R1
+JZ EQUAL           
+MOV 52H, #0FFH     
+SJMP STOP
+
+B_ZERO:
+MOV 52H, #01H      
+SJMP STOP
+
+EQUAL:
+MOV 52H, #00H      
+
+STOP:
+SJMP STOP
+END
